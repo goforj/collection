8c41921b9aa0b62317c4bef0bf73c62e54c9a7ca
Chris Miles <chris.miles.e@gmail.com>
2025-12-13 23:55:32 -0600

    docs: test

--- docs/readme/main.go
+++ docs/readme/main.go
@@ -274,12 +274,9 @@ func renderAPI(funcs []*FuncDoc) string {
 					buf.WriteString(fmt.Sprintf("_Example: %s_\n\n", ex.Label))
 				}
 
-				buf.WriteString("<details>\n")
-				buf.WriteString("<summary>Examples</summary>\n\n")
 				buf.WriteString("```go\n")
 				buf.WriteString(strings.TrimSpace(ex.Code))
 				buf.WriteString("\n```\n\n")
-				buf.WriteString("</details>\n\n")
 			}
 		}
 	}

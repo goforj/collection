50f7a703e0b68a47e0401de784b17ab3913932ae
Chris Miles <chris.miles.e@gmail.com>
2025-12-14 15:16:06 -0600

    docs: tweak

--- examples/chaining/main.go
+++ examples/chaining/main.go
@@ -22,16 +22,10 @@ func main() {
 	collection.
 		New(events). // Construction
 		Shuffle(). // Ordering
-		Filter(func(e DeviceEvent) bool {
-			return e.Errors > 5
-		}). // Slicing
-		Sort(func(a, b DeviceEvent) bool {
-			return a.Errors > b.Errors
-		}). // Ordering
+		Filter(func(e DeviceEvent) bool { return e.Errors > 5 }). // Slicing
+		Sort(func(a, b DeviceEvent) bool { return a.Errors > b.Errors }). // Ordering
 		Take(5). // Slicing
-		TakeUntilFn(func(e DeviceEvent) bool {
-			return e.Errors < 10
-		}). // Slicing (stop when predicate becomes true)
+		TakeUntilFn(func(e DeviceEvent) bool { return e.Errors < 10 }). // Slicing (stop when predicate becomes true)
 		SkipLast(1). // Slicing
 		Dump() // Debugging
 
